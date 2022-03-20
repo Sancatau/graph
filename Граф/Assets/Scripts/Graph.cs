d603fac045511a4fb3ea6bcf6c8d5548f9bd3472
Sancatau <rouale@mail.ru>
2022-03-20 20:22:02 +0300

    uncompiled.1
    надо доделать ввод функции с клавиатуры

---
+++ Граф/Assets/Scripts/Graph.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+
+            position.x = (i + 0.5f) * step - 1f;
+
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+
+            points[i] = point;
+        }
+    }
+
+    
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = 0.5f * Mathf.Sin(2f * Mathf.PI * (position.x + 0.5f * time));
+            point.localPosition = position;
+        }
+    }
+}
